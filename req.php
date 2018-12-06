15fda133fcda273430dd60b5bc914f2164695b01
Sam Wheeler <1700863@uad.ac.uk>
2018-12-06 22:30:46 +0000

    Final Tweaks

--- req.php
+++ req.php
@@ -12,7 +12,6 @@
     
     <p>The following is the list of requirements stated in the project brief and how each one has been achieved</p>
 
-<<<<<<< HEAD
     <table>
         <tr>
             <td>A clear use of HTML 5</td>
@@ -71,13 +70,13 @@
         </tr>
         <tr>
             <td>Ability to ADD/EDIT/DELETE information from a database</td>
-            <td></td>
-            <td></td>
+            <td>./lib/userAuth.php</td>
+            <td>Within this file there is the ability to ADD an account with password to the Database.</td>
         </tr>
         <tr>
             <td>Appropriate consideration of relevant laws</td>
-            <td></td>
-            <td></td>
+            <td>N/A</td>
+            <td>All Images have been provided for free on the web and sources have been included in this file, User data is protected with hashing and salting function. The user is also alerted to the use of cookies in the site.</td>
         </tr>
         <tr>
             <td>Security Measures (Prepared Statements/Passwords salted and hashed/valiation of user input/any other security features)</td>
@@ -85,53 +84,6 @@
             <td>Use of salting and hashing is included in the registration process in this page.</td>
         </tr>
     </table>
-=======
-    <ul>
-        <li>A clear use of HTML 5</li>
-        <p>index.php or any item in the components folder contains a use of HTML5.</p>
-
-        <li>Use of Bootstrap framework for a responsive layout</li>
-        <p>Header is responsive to device size and will change appearance to suit the user. this is using Bootstrap in navbar.php</p>
-
-        <li>Use of JavaScript to manipulate the DOM based on an Event</li>
-        <p>---</p>
-
-        <li>JavaScript loading of dynamically changing information</li>
-        <p>--- Highest played song on spotify API</p>
-
-        <li>Use of jQuery in conjunction with the DOM</li>
-        <p>Page scrolling function located in main.js scrolls smoothly from point to point based on event of navbar click. between SCROLL FUNC START and SCROLL FUNC END comments</p>
-        
-        <li>Use of jQuery plugin to enhance your appliction</li>
-        <p>---</p>
-        
-        <li>Use of AJAX</li>
-        <p> --- Spotify Request (Page Load)</p>
-        
-        <li>Use of the jQuery AJAX function</li>
-        <p>main.js form handling of login and register to return error and validate</p>
-        
-        <li>Use of Cookies</li>
-        <p>---</p>
-        
-        <li>User Login functionality (PHP/SQL)</li>
-        <p>---</p>
-        
-        <li>Admin Section of the site (PHP/SQL)</li>
-        <p>---</p>
-        
-        <li>Ability to ADD/EDIT/DELETE information from a database</li>
-        <p>---</p>
-        
-        <li>Appropriate consideration of relevant laws</li>
-        <p>---</p>
-        
-        <li>Security Measures (Prepared Statements/Passwords salted and hashed/valiation of user input/any other security features)</li>
-        <p>- SQL Prepared statement are included in ____</p>
-        <p>- hashing using sha2</p>
-        <p>- User input in both registration form and login for are validated in their appropriate lib php files</p>
-    </ul>
->>>>>>> 249209a4a2212a5fb3bbd4435b9eade82f658f04
     
     IMAGE SOURCES
     Logo: https://img00.deviantart.net/97e7/i/2010/301/0/0/pendulum_logo_by_ipodzanyman-d30bi0q.png
